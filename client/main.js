58142da196a26706f6810b084103647149bbed74
Better Futures <betterfuture032@gmail.com>
2023-01-29 11:53:13 -0600

    remove client

---
+++ client/main.js
@@ -0,0 +1,99 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+
+const loader = (element) => {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+};
+
+const typeText = (element, text) => {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+};
+
+const generateUniqueId = () => {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+};
+
+const chatStripe = (isAi, value, uniqueId) => {
+  return `
+      <div class="wrapper ${isAi && "ai"}">
+        <div class="chat">
+          <div class="profile">
+          <img src="${isAi ? bot : user}" alt='${isAi ? "bot" : "user"}' />
+        </div>
+        <div class="message" id=${uniqueId}>
+        ${value}
+        </div>
+      </div>
+    `;
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, '', uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  
+  const messageDiv = document.getElementById(`${uniqueId}`);
+
+  loader(messageDiv);
+
+  const response = await fetch("https://cea-ai.onrender.com", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ prompt: data.get("prompt") }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if(response.ok){
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  }else {
+    const error = await response.text();
+    messageDiv.innerHTML = `${error}`;
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => { 
+  if(e.key === 'Enter'){  
+    handleSubmit(e);
+  }
+});
